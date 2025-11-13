7d0e6bf338a68d06ef0a802470ca95972169c2b7
shahedali2001 <shahedali2001@gmail.com>
2025-11-13 20:26:50 +0530

    Update backend.tf

--- dev/backend.tf
+++ dev/backend.tf
@@ -1,3 +1,10 @@
 terraform {
-  backend "azurerm" {}
-   
+  backend "azurerm" {
+    storage_account_name = "shahedstg779"
+    container_name       = "tfscontainer"
+    key                  = "dev.terraform.tfstate"
+    resource_group_name  = "shahed-dev-01"
+    subscription_id      = "2c6da294-c904-4801-bffd-cd7f0b236e3d"
+    use_azuread_auth     = true
+  }
+}
