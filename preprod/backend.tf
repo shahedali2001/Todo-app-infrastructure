02334d0e95b8be93dc2154b5ae3716acf66a5031
shahedali2001 <shahedali2001@gmail.com>
2025-12-13 02:11:47 +0530

    Add files via upload

---
+++ preprod/backend.tf
@@ -0,0 +1,10 @@
+terraform {
+  backend "azurerm" {
+    storage_account_name = "libo4520"
+    container_name       = "tfscontainer"
+    key                  = "preprod.terraform.tfstate"
+    resource_group_name  = "libo-rg"
+    subscription_id      = "711f9f70-1892-49fb-a04f-5bf2c5a89677"
+    use_azuread_auth     = true
+  }
+}
---
+++ preprod/varaible.tf
@@ -0,0 +1,44 @@
+variable "rgs" {}
+variable "storages" {}
+variable "vnets" {}
+variable "nsgs" {}
+variable "vms" {}
+variable "pips" {}
+variable "key_vaults" {}
+variable "mssql_servers" {}
+variable "mssql_databases" {}
+variable "log_analytics_workspaces" {}
+variable "recovery_services_vaults" {}
+variable "key_vault_secret" {}
+variable "backup_policies" {}
+variable "shahed_lb" {}
+variable "shahed_lb_nics" {}
+variable "bastion_hosts" {}
+variable "application_gateways" {}
+# variable "subnet_ids" {}
+# variable "public_ip_ids" {}
+
+  
+ 
+
+  
+
+  
+
+
+  
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
