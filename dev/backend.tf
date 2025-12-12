5cf5ef8327dfc9de10e9b872ce20f40ee7081842
shahedali2001 <shahedali2001@gmail.com>
2025-12-12 20:18:51 +0530

    Update backend.tf

--- dev/backend.tf
+++ dev/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "azurerm" {
-    storage_account_name = "shahedstg1920"
+    storage_account_name = "libo4520"
     container_name       = "tfscontainer"
     key                  = "dev.terraform.tfstate"
-    resource_group_name  = "shahed-dev-01"
+    resource_group_name  = "libo-rg"
     subscription_id      = "711f9f70-1892-49fb-a04f-5bf2c5a89677"
     use_azuread_auth     = true
   }
