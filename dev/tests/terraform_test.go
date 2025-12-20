1ac2716d7a85bddafb7da2de89a718715dfa31d5
shahedali2001 <shahedali2001@gmail.com>
2025-12-20 15:39:41 +0530

    Add Terraform test for Azure deployment

---
+++ dev/tests/terraform_test.go
@@ -0,0 +1,15 @@
+package test
+
+import (
+  "testing"
+  "github.com/gruntwork-io/terratest/modules/terraform"
+)
+
+func TestTerraformAzure(t *testing.T) {
+  terraformOptions := &terraform.Options{
+    TerraformDir: "../dev",
+  }
+
+  defer terraform.Destroy(t, terraformOptions)
+  terraform.InitAndApply(t, terraformOptions)
+}
